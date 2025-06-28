6725d4466ea4b6e40a51a159c19fd538a244ed45
KENETH217 <kipyegonkeneth03@gmail.com>
2025-06-28 22:57:55 +0300

    Created an invoicing Application, add customer, add invoice, make payments, generate receipts and Generate a ledger for all transactions

---
+++ src/main/java/com/app/invoice/entity/QuotationItem.java
@@ -0,0 +1,31 @@
+package com.app.invoice.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class QuotationItem {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String description;
+    private int quantity;
+    private BigDecimal unitPrice;
+
+    @ManyToOne
+    @JsonIgnore
+    private Quotation quotation;
+}
