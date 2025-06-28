744ce4e5fd1eef42cd770c46d895cd89e0055812
KENETH217 <kipyegonkeneth03@gmail.com>
2025-06-28 22:53:17 +0300

    Created an invoicing Application, add customer, add invoice, make payments, generate receipts and Generate a ledger for all transactions

---
+++ src/main/java/com/app/invoice/entity/Customer.java
@@ -0,0 +1,33 @@
+package com.app.invoice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+    private String city;
+    private String state;
+    private String country;
+    private String zipCode;
+    private boolean deleted = false;
+
+
+}
