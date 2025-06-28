0b2bf2cb08c3320e41077404662f3e15f71f2f40
KENETH217 <kipyegonkeneth03@gmail.com>
2025-06-28 22:52:35 +0300

    Created an invoicing Application, add customer, add invoice, make payments, generate receipts and Generate a ledger for all transactions

---
+++ src/main/java/com/app/invoice/dto/CustomerResponse.java
@@ -0,0 +1,25 @@
+package com.app.invoice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerResponse {
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String country;
+    private boolean deleted;
+}
