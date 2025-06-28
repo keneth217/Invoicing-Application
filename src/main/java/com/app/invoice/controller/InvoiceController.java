ee3a0ee308f4c824bc9dc50aae63b3bfa08511f9
KENETH217 <kipyegonkeneth03@gmail.com>
2025-06-28 22:53:46 +0300

    Created an invoicing Application, add customer, add invoice, make payments, generate receipts and Generate a ledger for all transactions

---
+++ src/main/java/com/app/invoice/controller/InvoiceController.java
@@ -0,0 +1,41 @@
+package com.app.invoice.controller;
+
+import com.app.invoice.dto.InvoiceRequest;
+import com.app.invoice.dto.InvoiceResponse;
+import com.app.invoice.entity.Invoice;
+import com.app.invoice.service.InvoiceService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/invoices")
+public class InvoiceController {
+
+    private final InvoiceService invoiceService;
+
+    public InvoiceController(InvoiceService invoiceService) {
+        this.invoiceService = invoiceService;
+    }
+
+    @PostMapping
+    public ResponseEntity<InvoiceResponse> createInvoice(@RequestBody InvoiceRequest invoice) {
+        InvoiceResponse createdInvoice = invoiceService.createInvoice(invoice);
+        return ResponseEntity.status(201).body(createdInvoice); // 201 Created
+
+
+    }
+
+    @PostMapping("/all")
+    public ResponseEntity<List<InvoiceResponse>> getAllInvoices() {
+        List<InvoiceResponse> invoices = invoiceService.getAllInvoices();
+        return ResponseEntity.ok(invoices); // 200 OK
+    }
+    @PostMapping("/{id}/update")
+    public ResponseEntity<InvoiceResponse> updateInvoice(  @PathVariable Long id,@RequestBody InvoiceRequest invoice) {
+        InvoiceResponse updatedInvoice = invoiceService.updateInvoice(id,invoice);
+        return ResponseEntity.ok(updatedInvoice); // 200 OK
+    }
+
+}
