8a0d44b040f25765739e50cfe058f9713c2a8ba1
KENETH217 <kipyegonkeneth03@gmail.com>
2025-06-28 22:53:23 +0300

    Created an invoicing Application, add customer, add invoice, make payments, generate receipts and Generate a ledger for all transactions

---
+++ src/main/java/com/app/invoice/repo/InvoiceRepository.java
@@ -0,0 +1,19 @@
+package com.app.invoice.repo;
+
+import com.app.invoice.entity.Invoice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
+    @Query(value = "SELECT invoice_number FROM invoice ORDER BY id DESC LIMIT 1", nativeQuery = true)
+    String findLastInvoiceNumber();
+
+    List<Invoice> findAllByDeletedFalse();
+
+    Optional<Invoice> findByIdAndDeletedFalse(Long id);
+}
