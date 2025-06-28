c4fbd353b421e035c8bc5f57abc2bb350ccbd476
KENETH217 <kipyegonkeneth03@gmail.com>
2025-06-28 22:52:41 +0300

    Created an invoicing Application, add customer, add invoice, make payments, generate receipts and Generate a ledger for all transactions

---
+++ src/test/java/com/app/invoice/InvoiceApllicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.invoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceApllicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
