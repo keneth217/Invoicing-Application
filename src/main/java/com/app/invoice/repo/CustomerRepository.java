8659e7befe6354cc238628049128091b230eee1d
KENETH217 <kipyegonkeneth03@gmail.com>
2025-06-30 11:07:59 +0300

    Generate business apis

--- src/main/java/com/app/invoice/repo/CustomerRepository.java
+++ src/main/java/com/app/invoice/repo/CustomerRepository.java
@@ -3,7 +3,9 @@ package com.app.invoice.repo;
 import com.app.invoice.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    List<Customer> findAllByDeletedFalse();
 }
