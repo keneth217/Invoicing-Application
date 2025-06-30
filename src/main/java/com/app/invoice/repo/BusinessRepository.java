68db26fc69cc7cb47bae65a346bdb1736cec356a
KENETH217 <kipyegonkeneth03@gmail.com>
2025-06-30 11:07:55 +0300

    Generate business apis

---
+++ src/main/java/com/app/invoice/repo/BusinessRepository.java
@@ -0,0 +1,21 @@
+package com.app.invoice.repo;
+
+import com.app.invoice.entity.Business;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BusinessRepository extends JpaRepository<Business,Long> {
+    List<Business> findAllByDeletedFalse();
+
+    Optional<Business> findByBusinessCode(String businessCode);
+
+    @Query("SELECT b.businessCode FROM Business b WHERE b.businessCode IS NOT NULL ORDER BY b.businessCode DESC LIMIT 1")
+    String findLastBusinessCode();
+
+    boolean existsByName(String name);
+
+    boolean existsByBusinessCode(String businessCode);
+}
