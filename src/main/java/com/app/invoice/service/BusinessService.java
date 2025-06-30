537e0064c1deb27155d1c4adce5ad6c4aabdd17e
KENETH217 <kipyegonkeneth03@gmail.com>
2025-06-30 11:06:53 +0300

    Generate business apis

---
+++ src/main/java/com/app/invoice/service/BusinessService.java
@@ -0,0 +1,15 @@
+package com.app.invoice.service;
+
+import com.app.invoice.dto.BusinessRequest;
+import com.app.invoice.dto.BusinessResponse;
+
+import java.util.List;
+
+public interface BusinessService {
+    BusinessResponse createCompany(BusinessRequest businessRequest);
+    List<BusinessResponse> getAllCompanies();
+    BusinessResponse getCompanyById(Long id);
+    BusinessResponse updateCompany(Long id, BusinessRequest businessRequest);
+    BusinessResponse deleteCompany(Long id);
+    BusinessResponse getCompanyByCode(String businessCode);
+}
