3da6427dfaeb60ca3cf40b2fa805a1901e2a67b6
KENETH217 <kipyegonkeneth03@gmail.com>
2025-07-01 21:08:59 +0300

    Populate the dashboard data .

--- src/main/java/com/app/invoice/repo/InvoiceRepository.java
+++ src/main/java/com/app/invoice/repo/InvoiceRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,4 +18,9 @@ public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
     List<Invoice> findAllByDeletedFalse();
 
     Optional<Invoice> findByIdAndDeletedFalse(Long id);
+
+    long countByDueDateBeforeAndStatusNot(LocalDate date, String status);
+    long countByDueDateBeforeAndStatus(LocalDate date, String status);
+    long countByDueDateAfter(LocalDate date);
+    List<Invoice> findTop5ByOrderByIssueDateDesc();
 }
