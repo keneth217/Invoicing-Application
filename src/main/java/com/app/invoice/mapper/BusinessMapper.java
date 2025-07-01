b26036d86a09a366db6be5edb771bbf70aeefccb
KENETH217 <kipyegonkeneth03@gmail.com>
2025-07-01 21:08:21 +0300

    Populate the dashboard data .

--- src/main/java/com/app/invoice/mapper/BusinessMapper.java
+++ src/main/java/com/app/invoice/mapper/BusinessMapper.java
@@ -5,7 +5,7 @@ import com.app.invoice.dto.BusinessResponse;
 import com.app.invoice.entity.Business;
 
 public class BusinessMapper {
-    public  static Business toCompanyEntity(BusinessRequest company) {
+    public static Business toCompanyEntity(BusinessRequest company) {
         return Business.builder()
                 .name(company.getName())
                 .email(company.getEmail())
@@ -17,10 +17,22 @@ public class BusinessMapper {
                 .zipCode(company.getZipCode())
                 .country(company.getCountry() != null ? company.getCountry() : "Kenya")
                 .imageUrl(company.getImageUrl())
+                .invoicePrefix(company.getInvoicePrefix())
+                .invoiceFooter(company.getInvoiceFooter())
+                .invoiceTerms(company.getInvoiceTerms())
+                .invoiceNote(company.getInvoiceNote())
+                .receiptPrefix(company.getReceiptPrefix())
+                .receiptFooter(company.getReceiptFooter())
+                .receiptTerms(company.getReceiptTerms())
+                .receiptNote(company.getReceiptNote())
+                .voucherPrefix(company.getVoucherPrefix())
+                .currencySymbol(company.getCurrencySymbol())
+                .website(company.getWebsite())
+                .deleted(false)
                 .build();
     }
 
-    public  static BusinessResponse toCompanyResponse(Business company) {
+    public static BusinessResponse toCompanyResponse(Business company) {
         return new BusinessResponse(
                 company.getId(),
                 company.getName(),
@@ -32,7 +44,18 @@ public class BusinessMapper {
                 company.getState(),
                 company.getZipCode(),
                 company.getCountry(),
-                company.getImageUrl()
+                company.getImageUrl(),
+                company.getInvoicePrefix(),
+                company.getInvoiceFooter(),
+                company.getInvoiceTerms(),
+                company.getInvoiceNote(),
+                company.getReceiptPrefix(),
+                company.getReceiptFooter(),
+                company.getReceiptTerms(),
+                company.getReceiptNote(),
+                company.getVoucherPrefix(),
+                company.getCurrencySymbol()
+
 
         );
     }
