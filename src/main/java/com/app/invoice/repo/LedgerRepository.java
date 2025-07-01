efbb968d9556d4a448978bf051c3032750cd999c
KENETH217 <kipyegonkeneth03@gmail.com>
2025-07-01 22:08:24 +0300

    update ledger transaction

--- src/main/java/com/app/invoice/repo/LedgerRepository.java
+++ src/main/java/com/app/invoice/repo/LedgerRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 
 @Repository
@@ -17,6 +18,8 @@ public interface LedgerRepository extends JpaRepository<LedgerEntry, Long> {
     @Query("SELECT l.type, SUM(CASE WHEN l.type = 'DEBIT' THEN l.debitAmount ELSE l.creditAmount END) FROM LedgerEntry l WHERE l.deleted = false GROUP BY l.type")
     List<Object[]> getTotalDebitsAndCredits();
     List<LedgerEntry> findAllByDeletedFalseOrderByTransactionDateAsc();
+
+    List<LedgerEntry> findTop5ByOrderByTransactionDateDesc();
 
 //    List<LedgerEntry> findByDescriptionContainingIgnoreCaseOrReferenceContainingIgnoreCase(String desc, String ref);
 
